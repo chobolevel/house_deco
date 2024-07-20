fad50d49601ef3553e85ae3a9418efa72726ffe8
chobolevel <rodaka123@naver.com>
2024-07-20 13:27:33 +0900

    Fix: deleted product option fetching issue

--- domain/src/main/kotlin/com/movieland/domain/entity/product/option/ProductOptionFinder.kt
+++ domain/src/main/kotlin/com/movieland/domain/entity/product/option/ProductOptionFinder.kt
@@ -13,7 +13,7 @@ class ProductOptionFinder(
 
     @Throws(DataNotFoundException::class)
     fun findById(id: Long): ProductOption {
-        return repository.findByIdOrNull(id) ?: throw DataNotFoundException(
+        return repository.findByIdAndDeletedFalse(id) ?: throw DataNotFoundException(
             errorCode = ErrorCode.PRODUCT_OPTION_IS_NOT_FOUND,
             message = "상품 옵션 정보를 찾을 수 없습니다."
         )
